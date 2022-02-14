a13a08ad5e4ae74c23d5172cf4689b85afa2019d
orbich <orbidan@bk.ru>
2022-02-15 01:51:04 +0300

    lesson #3

---
+++ Lesson_03/client.py
@@ -0,0 +1,57 @@
+"""Программа-клиент"""
+
+import socket
+import sys
+import time
+from common.utils import *
+from common.variables import *
+
+
+# сформировать presence-сообщение
+def create_presence(account_name='Guest'):
+    out = {
+        ACTION: PRESENCE,
+        TIME: time.time(),
+        USER: {
+            ACCOUNT_NAME: account_name
+        }
+    }
+    return out
+
+
+# обработать ответ сервера
+def process_ans(message):
+    if RESPONSE in message:
+        if message[RESPONSE] == 200:
+            return '200: OK'
+        return f'400: {message[ERROR]}'
+    raise ValueError
+
+
+def main():
+    # ---параметры командной строки скрипта client.py <addr> [<port>]---
+    try:
+        server_address = sys.argv[1]
+        server_port = int(sys.argv[2][1:-1])
+        if server_port < 1024 or server_port > 65535:
+            raise ValueError
+    except IndexError:
+        server_address = DEFAULT_IP_ADDRESS
+        server_port = DEFAULT_PORT
+    except ValueError:
+        print('В качестве порта может быть указано число в диапазоне от 1024 до 65535')
+        sys.exit(1)
+
+    transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    transport.connect((server_address, server_port))
+    message_to_server = create_presence()
+    send_message(transport, message_to_server)
+    try:
+        answer = process_ans(get_message(transport))
+        print(answer)
+    except (ValueError, json.JSONDecodeError):
+        print('Не удалось декодировать сообщение сервера.')
+
+
+if __name__ == '__main__':
+    main()
