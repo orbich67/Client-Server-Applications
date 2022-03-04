ebedcd835cc212b10c2c4a2eeb98f1c41d19e322
orbich <orbidan@bk.ru>
2022-03-04 22:57:32 +0300

    lesson #6

---
+++ Lesson_06/common/variables.py
@@ -0,0 +1,23 @@
+import logging
+
+# порт по умолчанию:
+DEFAULT_PORT = 7777
+# IP адрес по умолчанию для подключения клиента:
+DEFAULT_IP_ADDRESS = '127.0.0.1'
+# максимальная очередь подключений:
+MAX_CONNECTIONS = 5
+# Максимальная длина сообщения (байт):
+MAX_PACKAGE_LENGTH = 1024
+# Кодировка проекта
+ENCODING = 'utf-8'
+# Текущий уровень логирования
+LOGGING_LEVEL = logging.DEBUG
+
+# Протокол JIM (ключи)
+ACTION = 'action'
+TIME = 'time'
+USER = 'user'
+ACCOUNT_NAME= 'account_name'
+PRESENCE = 'presence'
+RESPONSE = 'response'
+ERROR = 'error'
